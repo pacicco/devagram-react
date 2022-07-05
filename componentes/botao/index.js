e9abcccde4f5ea58a52c3de277dc2b279b72a9ae
pacicco <102540905+pacicco@users.noreply.github.com>
2022-07-05 01:36:50 -0300

    instalação libs, criação imagens, upload e avatar

---
+++ componentes/botao/index.js
@@ -0,0 +1,18 @@
+export default function Botao({
+    type = 'button',
+    texto,
+    cor = 'primaria',
+    desabilitado = false,
+    manipularClique
+}) {
+    return (
+        <button
+        type = {type}
+        className={`btn ${cor}`}
+        disabled = {desabilitado}
+        onClick = {manipularClique}
+        >
+            {texto}
+        </button>
+    );
+}
