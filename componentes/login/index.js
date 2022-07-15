3da9fbf069abc38d398661437cf1bea20fef0462
pacicco <102540905+pacicco@users.noreply.github.com>
2022-07-15 17:19:55 -0300

    páginas de login e cadastro

---
+++ componentes/login/index.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import InputPublico from "../inputPublico";
+import Botao from "../botao";
+import imagemEnvelope from "../../public/images/imagemEnvelope.svg";
+import chave from "../../public/images/chave.svg";
+import logo from "../../public/images/logo.svg"
+
+
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+
+
+    return (
+        <section className={`paginaLogin paginaPublica`}>
+            <div className="logoContainer">
+                <Image
+                    src={logo}
+                    alt="logotipo"
+                    layout="fill"
+                    className="logo"
+                />
+            </div>
+
+            <div className="conteudoPaginaPublica">
+                <form>
+
+                    <InputPublico
+                        imagem={imagemEnvelope}
+                        texto="E-mail"
+                        tipo="email"
+                        aoAlterarValor={e => setEmail(e.target.value)}
+                        valor={email}
+                    />
+                    <InputPublico
+                        imagem={chave}
+                        texto="senha"
+                        tipo="password"
+                        aoAlterarValor={e => setSenha(e.target.value)}
+                        valor={senha}
+                    />
+
+                    <Botao
+                        texto="login"
+                        tipo="submit"
+                        desabiltado={false}
+
+                    />
+                </form>
+
+                <div className="rodapePaginaPublica">
+                    <p>Não possui uma conta?</p>
+                    <Link href="/cadastro">Faça seu cadastro agora</Link>
+
+                </div>
+
+
+            </div>
+
+        </section>
+
+    );
+}
---
+++ pages/cadastro/index.js
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import logo from "../../public/images/logo.svg"
+import Image from "next/image";
+import Link from "next/link";
+import Botao from "../../componentes/botao";
+import InputPublico from "../../componentes/inputPublico";
+import uploadImagem from "../../componentes/uploadImagem"
+
+import usuario from "../../public/images/usuario.svg"
+import imagemEnvelope from "../../public/images/imagemEnvelope.svg"
+import chave from "../../public/images/chave.svg"
+import avatar from "../../public/images/avatar.svg"
+
+
+
+
+export default function Cadastro() {
+    const [imagem, setImagem] = useState (null);
+    const [email, setEmail] = useState ("");
+    const [nome, setNome] = useState ("");
+    const [senha, setSenha] = useState ("");
+    const [confirmacaoSenha, setConfirmacaoSenha] = useState ("");
+
+
+    return (
+        <section className={`paginaCadastro paginaPublica`}>
+            <div className="logoContainer desktop">
+                <Image
+                    src={logo}
+                    alt="logotipo"
+                    layout="fill"
+                    className="logo"
+                />
+            </div>
+
+            <div className="conteudoPaginaPublica">
+                <form>
+                    <uploadImagem
+                        imagemPreviewClassName="avatar avatarPreview"
+                        imagemPreview={imagem?.preview || avatar.src}
+                        setImagem={setImagem}
+                    />
+                    <InputPublico
+                        imagem={usuario}
+                        texto="Nome Completo"
+                        tipo="text"
+                        aoAlterarValor={e => setNome(e.target.value)}
+                        valor={nome}
+                    />
+                    <InputPublico
+                        imagem={imagemEnvelope}
+                        texto="email"
+                        tipo="email"
+                        aoAlterarValor={e => setEmail(e.target.value)}
+                        valor={email}
+                    />
+                    <InputPublico
+                        imagem={chave}
+                        texto="senha"
+                        tipo="password"
+                        aoAlterarValor={e => setSenha(e.target.value)}
+                        valor={senha}
+                    />
+                    <InputPublico
+                        imagem={chave}
+                        texto="Confirmar senha"
+                        tipo="password"
+                        aoAlterarValor={e => setConfirmacaoSenha(e.target.value)}
+                        valor={confirmacaoSenha}
+                    />
+                <Botao
+                        texto="Cadastrar"
+                        tipo="submit"
+                        desabiltado={false}
+
+                    />
+                </form>
+            <div className="rodapePaginaPublica">
+                    <p>Já possui uma conta?</p>
+                    <Link href="/">Faça seu login agora</Link>
+
+                </div>
+            </div>
+        </section>
+    )
+} 
