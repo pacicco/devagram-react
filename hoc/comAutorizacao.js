ca321da1af8bc4096c0508428eea9fff16ea8a8b
pacicco <102540905+pacicco@users.noreply.github.com>
2022-08-05 12:22:02 -0300

    postagem, feed, comentario e loading

--- hoc/comAutorizacao.js
+++ hoc/comAutorizacao.js
@@ -1,15 +1,38 @@
 import { useRouter } from "next/router";
-import Cabecalho from "../componentes/layout/";
-import Rodape from "../componentes/layout/Rodape.js";
+import Cabecalho from "../componentes/layout/Cabecalho";
+import Rodape from "../componentes/layout/Rodape";
 import Loading from "../componentes/loading";
 import UsuarioService from "../services/UsuarioService";
 
 
 
-const usuarioService = new UsuarioService ();
+const usuarioService = new UsuarioService();
 
 
-export default function
+export default function comAutorizacao(Componente) {
+    return (props) => {
+        const router = useRouter();
+
+        if (typeof window !== 'undefined') {
+            if (!usuarioService.estaAutenticado()) {
+                router.replace('/');
+                return null;
+            }
+
+            const usuarioLogado = usuarioService.obterInformacoesUsuarioLogado();
+
+            return (
+                <>
+                    <Cabecalho usuarioLogado={usuarioLogado} />
+                    <Loading />
+                    <Componente usuarioLogado={usuarioLogado}{...props} />
+                    <Rodape usuarioLogado={usuarioLogado} />
+                </>
+            );
+        }
+        return null;
+    }
+
+}
 
 
-//fazer pasta layout dentro da pasta componentes (e demais pastas)
